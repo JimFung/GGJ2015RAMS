4fd36aff0f323eb83ca69720e31eef97cfe0bba6
German <vo.german@gmail.com>
2015-01-24 13:39:27 -0800

    script exception

---
+++ RAMS/Assets/Scripts/PlatformMoveScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformMoveScript : MonoBehaviour {
+
+	[SerializeField] Rigidbody2D _grassPlatform;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_grassPlatform.AddForce(Vector3.up);
+
+	}
+
+	void FixedUpdate() {
+
+	}
+}
+
