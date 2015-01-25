16229c0d9b1a15f9ca07891110883037ed30a2c0
JimFung <jfung92@gmail.com>
2015-01-25 02:26:49 -0800

    clouds animate and scripted

---
+++ RAMS/Assets/Scripts/FlipClouds.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlipClouds : MonoBehaviour {
+
+	public void randomizeCloud(){
+		this.gameObject.transform.localScale = new Vector3 ((-1 * this.gameObject.transform.localScale.x), this.gameObject.transform.localScale.y, this.gameObject.transform.localScale.z);
+		//layer = 2 : behind mountain
+		//layer = 5 : infront of mountain
+		Debug.Log (this.gameObject.transform.localScale);
+		if((int)Random.Range(1,10)%2 == 0){
+			//even
+			this.gameObject.renderer.sortingOrder= 2;
+		}else{
+			//odd
+			this.gameObject.renderer.sortingOrder = 5;
+		}
+		Debug.Log (this.gameObject.renderer.sortingLayerName);
+		Debug.Log (this.gameObject.renderer.sortingOrder);
+	}
+}
