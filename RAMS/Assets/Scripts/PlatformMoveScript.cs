71c17a2a8bc669c5013841ad97771b52732c0299
German <vo.german@gmail.com>
2015-01-24 20:48:16 -0800

    Adding errthang

--- RAMS/Assets/Scripts/PlatformMoveScript.cs
+++ RAMS/Assets/Scripts/PlatformMoveScript.cs
@@ -3,21 +3,41 @@ using System.Collections;
 
 public class PlatformMoveScript : MonoBehaviour {
 
-	[SerializeField] Rigidbody2D _grassPlatform;
+	[SerializeField] Transform platform;
+	[SerializeField] Transform startTransform;
+	[SerializeField] Transform endTransform;
+	[SerializeField] float platformSpeed;
+	[SerializeField] int stillTimeSeconds;
 
-	// Use this for initialization
-	void Start () {
-	
+	Vector3 direction;
+	Transform destination;
+
+	void Start() {
+		SetDestination (startTransform);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		_grassPlatform.AddForce(Vector3.up);
 
-	}
-
+	//define when to run in project settings time
 	void FixedUpdate() {
+		platform.rigidbody2D.MovePosition (platform.position + direction * platformSpeed * Time.fixedDeltaTime);
+		float nextMove;
+		if (Vector3.Distance (platform.position, destination.position) < platformSpeed * Time.fixedDeltaTime) {
+			nextMove = Time.time + stillTimeSeconds;
+			//while(Time.time < nextMove) { continue; }
+			SetDestination (destination == startTransform ? endTransform : startTransform);
+		}
+	}
 
+	void SetDestination(Transform dest) {
+
+		destination = dest;
+		direction = (destination.position - platform.position).normalized;
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireCube (startTransform.position, platform.localScale);
+
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireCube (endTransform.position, platform.localScale);
 	}
 }
-
