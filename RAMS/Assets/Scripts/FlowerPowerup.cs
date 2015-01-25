5f30319a8a409f02ba837089275b23d6b0e82c37
DangDavid <daviddang.92@gmail.com>
2015-01-25 11:17:25 -0800

    update dead sound clips, and started on powers

---
+++ RAMS/Assets/Scripts/FlowerPowerup.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlowerPowerup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D (Collision2D hit) {
+
+		var hitLayer = hit.gameObject.layer;
+		// 8 terrain, 9 goat, 10 goatpassableterrain layer
+		if ( hitLayer ==9 || hitLayer ==10  ){
+			
+			
+
+			
+			hit.gameObject.BroadcastMessage("AdjustCurrentHealth", 20);
+			this.gameObject.SetActive(false);
+			
+		}
+		
+	}
+}
---
+++ RAMS/Assets/Scripts/PowerUpHandler.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpHandler : MonoBehaviour {
+
+
+	[SerializeField] GameObject flower;
+	static System.Random rnd = new System.Random();
+	// spawning regions
+
+	private float minx = -7.0f;
+	private float maxx = -8.0f;
+	private float miny = 6.0f;
+	private float maxy = 8.0f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!flower.activeSelf && ChanceToOccur()) {
+			flower.transform.position = GetSpawnLocation();
+			flower.SetActive(true);
+		}
+	}
+
+
+	bool ChanceToOccur(){
+		int chance = rnd.Next(1200);
+		// chance == 0 for 1 in 30 secs in 60 fps chance of running
+		return chance == 0;
+
+	}
+	Vector3 GetSpawnLocation() {
+	
+		return new Vector3(Random.Range(minx, maxx), Random.Range(miny, maxy), 0);
+	
+	}
+}
+
