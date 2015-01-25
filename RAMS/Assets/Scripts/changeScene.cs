5c44aad1ba7437e2972a86c3a2a2ca2aa711cd6d
JimFung <jfung92@gmail.com>
2015-01-25 05:40:16 -0800

    made own timer, crashes unity.

--- RAMS/Assets/Scripts/changeScene.cs
+++ RAMS/Assets/Scripts/changeScene.cs
@@ -3,11 +3,21 @@ using System.Collections;
 
 public class changeScene : MonoBehaviour {
 
+	[SerializeField] AudioClip select;
 
+	public static bool flag = false;
+	public static float startTime = 0;
 	public void changeToScene(int scene){
 		if (scene == -1) {
 			Application.Quit();		
 		}
+		audio.PlayOneShot(select);
+		startTime = Time.time;
+		while (!flag) {
+			//updates inside SubmitLoop
+		}
+		flag = false;
 		Application.LoadLevel(scene);
 	}
+
 }
---
+++ RAMS/Assets/SubmitLoop.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SubmitLoop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time - changeScene.startTime >= 3) {
+			changeScene.flag = true;
+		}
+	}
+}
