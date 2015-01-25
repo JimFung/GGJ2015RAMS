0bd3bae2e15add254b92823539f9c2650013efc6
German <vo.german@gmail.com>
2015-01-24 23:49:36 -0800

    small improvements

--- RAMS/Assets/Scripts/HealthBarScript.cs
+++ RAMS/Assets/Scripts/HealthBarScript.cs
@@ -3,20 +3,23 @@ using System.Collections;
 
 public class HealthBarScript : MonoBehaviour {
 
+	[SerializeField] string position = "Left";
 	private int maxHealth = 100;
 	private int currHealth = 100;
+
+	private int barHeight = 20;
+	private int topPadding = 10;
+	private int x, y;
 
 	private float healthBarLength;
 
 	void OnGUI() {
-		GUI.Box(new Rect(10, 10, healthBarLength, 20), currHealth + "/" + maxHealth);
-	} 
+		GUI.Box(new Rect(x, y, healthBarLength, barHeight), currHealth + "/" + maxHealth);
+	}
 
 	public void AdjustCurrentHealth(int adj) {
-		int tmpHealth = currHealth;
+		//int tmpHealth = currHealth;
 		currHealth += adj;
-
-
 
 		if (currHealth < 0) {
 			currHealth = 0;
@@ -35,16 +38,15 @@ public class HealthBarScript : MonoBehaviour {
 //		while (tmpHealth != currHealth) {
 			healthBarLength = (Screen.width / 2) * (currHealth / (float)maxHealth);	
 //		}
-
 	}
 	
 	// Use this for initialization
 	void Start () {
-		healthBarLength = Screen.width / 2;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		AdjustCurrentHealth (0);
+		x = 10; y = topPadding;
+		healthBarLength = Screen.width / 2 - 2 * topPadding;
+
+		if (position.ToLower ().Equals ("right")) {
+			x = (int)(Screen.width - healthBarLength) - x;
+		}
 	}
 }
